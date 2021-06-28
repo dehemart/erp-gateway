3a57d0958e175a5e502f7f6ac95202e8c6246925
dehemart <martins.denis@gmail.com>
2021-06-28 12:27:33 -0300

    Init erp-gateway

---
+++ src/main/java/dehemart/erpgateway/ErpGatewayApplication.java
@@ -0,0 +1,14 @@
+package dehemart.erpgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class ErpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dehemart/erpgateway/ErpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dehemart.erpgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
